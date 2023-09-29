5bd0ef8df863c41de1ca93529920339c3fe301d0
den <holdenn119@gmail.com>
2023-09-29 10:24:28 +0100

    commit

--- gold/settings.py
+++ gold/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -121,6 +121,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+django_heroku.settings(locals())
 
 
 # Default primary key field type
